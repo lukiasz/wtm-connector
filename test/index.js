865d62f5d784c3ce795407843d0ccd823aaaf38b
Lukasz Olender <lukiasz@gmail.com>
2014-09-16 01:38:29 +0200

    account, content controllers support. written some tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,46 @@
-var should = require('chai').should();
-describe('hello npm!', function() {
-  it('check hello npm', function() {
-    'hello npm!'.should.equal('hello npm!');
-  })
+/* global require, describe, it, console */
+
+// line below allow to put alone expression in line, eg. 'expect(cookieJar).to.exist;'
+/* jshint -W030 */
+
+var expect = require('chai').expect;
+var wtmConnector = require('../wtm-connector.js');
+
+
+// in case you get a timeout: https://github.com/visionmedia/mocha/issues/1127
+describe('WtmConnector', function() {
+    var globalTestData = {
+        server: 'http://localhost:64053',
+        login: 'mister',
+        password: 'mister1'
+    };
+    
+    // it'll use 'server' property only
+    wtmConnector.init(globalTestData);
+    
+    describe('login', function() {
+        var testData = globalTestData;
+        it('should log in and save cookie when credentials are valid', function(done) {
+            this.timeout(10000);
+            wtmConnector.account.login(testData.login, testData.password).then(function(response) {
+                var cookieJar = wtmConnector.config.cookieJar;
+                var server = wtmConnector.config.server;
+                expect(server).to.be.a('string');
+                expect(server).to.equal(testData.server);
+                expect(cookieJar).to.exist;
+                var cookie = cookieJar.getCookieString(server);
+                expect(cookie).to.be.not.empty;
+                //expect(cookie).to.be.empty;
+                done();
+            });
+        });
+        
+        it('should throw unauthorized when credentials are not valid', function(done) {
+            this.timeout(10000);
+            wtmConnector.account.login(testData.login + 'xxx', testData.password).fail(function(error) {
+                expect(error.status).to.equal(400);
+                done();
+            });
+    	});
+	});
 });
--- wtm-connector.js
+++ wtm-connector.js
@@ -1,4 +1,145 @@
-var fs = require('fs');
-module.exports = function() {
-  console.log('hello, npm!');
-}
+/* global require, module */
+var unirest = require('unirest'),
+    q = require('q');
+
+var wtmConnector = function() {
+
+    var config = { };
+
+    var init = function(options) {
+        config.server = options.server || 'http://waytomaster.com';
+        config.cookieJar = options.cookieJar || unirest.jar();
+    };
+
+    // Account
+    
+    var login = function(login, password) {
+        var path = '/Account/Login';
+        var data = {
+            Name: login,
+            Password: password,
+            RememberMe: true
+        };
+        return post(path, data, initCookieJar);
+    };
+
+    var register = function(login, password, email) {
+        var path = '/Account/Register';
+        var data = {
+            Name: login,
+            Password: password,
+            Email: email
+        };
+        return post(path, data, initCookieJar);
+    };
+    
+    var getUserData = function() {
+        return get('/Account/GetUserData');
+    };
+    
+    var logoff = function() {
+        return get('/Account/LogOff');
+    };
+    
+    var isAuthenticated = function() {
+        return get('/Account/IsAuthenticated');
+    };
+    
+    // Content
+    
+    var index = function() {
+        return get('/Content/Index');
+    };
+    
+    var details = function(id) {
+        return get('/Content/Details?id=' + id);
+    };
+    
+    var getFile = function(id, removeKnownDialogs, srtOutput) {
+        var options = 'None';
+        if (removeKnownDialogs === true) {
+            options += ',RemoveKnownDialogs';
+        }
+        if (srtOutput === true) {
+            options += ',SrtOutput';
+        }
+        return get('/Content/Get?id=' + id + '&options=' + options);
+    };
+    
+    var uploadFile = function(filePath) {
+        var path = '/Content/New';
+        var data = 'notUsedHashcode';
+        return post(path, data, null, null, filePath);
+    };
+    
+    // Helpers methods
+    
+    var get = function(pathWithParams, success, error) {
+        var deferred = q.defer();
+        unirest.get(config.server + pathWithParams).jar(config.cookieJar).end(
+            function(response) {
+                if (response.error) {
+                    if (typeof error == typeof Function) {
+                        error(response.error);
+                    }
+                    deferred.reject(response.error);
+                } else {
+                    if (typeof success == typeof Function) {
+                        success(response);
+                    }
+                    deferred.resolve(response);
+                }
+            });
+        return deferred.promise;
+    };
+    
+    var post = function(path, data, success, error, filePath) {
+        var deferred = q.defer();
+        var call = unirest.post(config.server + path).send(data);
+        if (filePath) {
+            call.attach('file', filePath);
+        }
+        
+        call.jar(config.cookieJar).end(
+            function(response) {
+                if (response.error) {
+                    if (typeof error == typeof Function) {
+                        error(response.error);
+                    }
+                    deferred.reject(response.error);
+                } else {
+                    if (typeof success == typeof Function) {
+                        success(response);
+                    }
+                    deferred.resolve(response);
+                }
+            });
+        return deferred.promise;
+    };
+    
+    var initCookieJar = function(response) {
+        config.cookieJar = unirest.jar();
+        var authCookie = unirest.cookie('.ASPXAUTH=' +
+                                        response.cookies['.ASPXAUTH']);
+        config.cookieJar.add(authCookie, config.server);
+    };
+    
+    return {
+        init: init,
+        config: config,
+        account: {
+            login: login,
+        	getUserData: getUserData,
+            register: register,
+            isAuthenticated: isAuthenticated
+        },
+        content: {
+            index: index,
+            details: details,
+            get: getFile,
+            new: uploadFile
+        }
+    };
+};
+
+module.exports = new wtmConnector();
